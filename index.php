7632bc5a144c5ca6817969cfef1ef9960978446d
ClemMarkey <markey.clement@gmail.com>
2019-07-18 12:03:41 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Les Chouettes</title>
+</head>
+
+<body>
+    <!-- INC HEADER PHP -->
+
+    <main>
+        <h1>Les Chouettes</h1>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet doloremque dolorem quae eaque necessitatibus qui quis libero, ipsam molestias numquam possimus sequi. Quia quisquam saepe quis ullam perspiciatis, modi vitae.
+        </p>
+    </main>
+
+    <!-- INC FOOTER PHP -->
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+
+</html>
